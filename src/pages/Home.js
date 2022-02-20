fa23d0e4bdd831bf5b035cedfef6881348d81e10
ashish <aa54035@yahoo.com>
2022-02-20 18:51:05 +0530

    Addition of Router , Home page

---
+++ src/pages/Home.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+       <div className="container">
+
+
+        <div class="d-flex text-center 
+      justify-content-center align-items-center  flex-column text-area">
+
+
+          <h1 className="display-4 text-primary">
+            My First Tailwind CSS project
+        </h1>
+          <p >
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis, quam maiores facere
+            officiis harum delectus molestiae facilis est,
+            illum pariatur soluta? Voluptatibus odit sequi eligendi, a rem nostrum repudiandae porro?
+       </p>
+
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis, quam maiores facere
+            officiis harum delectus molestiae facilis est,
+            illum pariatur soluta? Voluptatibus odit sequi
+            eligendi, a rem nostrum repudiandae porro?
+            hi there
+       </p>
+
+
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis, quam maiores facere
+            officiis harum delectus molestiae facilis est,
+            illum pariatur soluta? Voluptatibus odit sequi eligendi, a rem nostrum repudiandae porro?
+       </p>
+        </div>
+
+
+      </div>
+
+   );
+}
+
+export default Home;
